21671b77e291b14e743eadbcebaafe5778fb5f63
Caio Nascimento Caminha <61422380+Caioncaminha@users.noreply.github.com>
2025-03-19 09:09:56 -0300

    Written "FIAP" with Morse Code on Arduino (LED)

--- sketch_mar18a.ino
+++ sketch_mar18a.ino
@@ -1,22 +1,63 @@
-int ledY = 9;
+int ledY = 8;
 int x = 0;
 
+//F . . _ .
+//I . .
+//A . _
+//P . _ _ .
+
 void setup() {
- pinMode(ledY, OUTPUT);
+pinMode(ledY, OUTPUT);
+
 }
 
 void loop() {
- for(x = 0; x < 3; x++){ //Letra S
+  delay(1500);
+ for(x = 0; x < 2; x++){ //Letra F
   digitalWrite(ledY, HIGH);
   delay(100);
   digitalWrite(ledY, LOW);
   delay(500);
  }
-
- for(x = 0; x < 3; x++){ //Letra O
   digitalWrite(ledY, HIGH);
-  delay(1000);
+  delay(800);
+  digitalWrite(ledY, LOW);
+  delay(500);
+  digitalWrite(ledY, HIGH);
+  delay(100);
+  digitalWrite(ledY, LOW);
+  delay(1500);
+ 
+ for(x = 2; x < 4; x++){ //Letra I
+  digitalWrite(ledY, HIGH);
+  delay(100);
   digitalWrite(ledY, LOW);
   delay(500);
  }
+  delay(1500);
+ for(x = 4; x < 5; x++){ //Letra A
+  digitalWrite(ledY, HIGH);
+  delay(100);
+  digitalWrite(ledY, LOW);
+  delay(500);
+  digitalWrite(ledY, HIGH);
+  delay(800);
+  digitalWrite(ledY, LOW);
+  delay(1500);
+ }
+
+  digitalWrite(ledY, HIGH);
+  delay(100);
+  digitalWrite(ledY, LOW);
+  delay(500);
+ for(x = 5; x < 7; x++){ //Letra P
+  digitalWrite(ledY, HIGH);
+  delay(800);
+  digitalWrite(ledY, LOW);
+  delay(500);
+ }
+  digitalWrite(ledY, HIGH);
+  delay(100);
+  digitalWrite(ledY, LOW);
+  delay(1500);
 }
