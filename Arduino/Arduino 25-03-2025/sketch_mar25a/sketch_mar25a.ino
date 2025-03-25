5dcb7f7da96d957ad26bb31cc4a605d0a040faaf
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-25 11:23:41 -0300

    Arduino Distance Model

---
+++ Arduino/Arduino 25-03-2025/sketch_mar25a/sketch_mar25a.ino
@@ -0,0 +1,42 @@
+#define trig 7
+#define echo 8
+int dist = 0;
+int ledG = 10;
+int ledY = 11;
+int ledR = 12;
+
+void setup() {
+  pinMode(trig, OUTPUT);
+  pinMode(ledG, OUTPUT);
+  pinMode(ledY, OUTPUT);
+  pinMode(ledR, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  dist = pulseIn(echo, HIGH);
+  dist = dist / 58;
+  Serial.println("distância: "+ String(dist) + String("cm"));
+  delay(2000);
+
+  if(dist<=15){
+    digitalWrite(ledR, HIGH);
+    digitalWrite(ledY, LOW);
+    digitalWrite(ledG, LOW);
+  }
+  else if(dist>15&&dist<=30){
+    digitalWrite(ledR, LOW);
+    digitalWrite(ledY, HIGH);
+    digitalWrite(ledG, LOW);
+    }
+  else if(dist>30&&dist<=400);{
+    digitalWrite(ledR, LOW);
+    digitalWrite(ledY, LOW);
+    digitalWrite(ledG, HIGH);
+  }
+}
