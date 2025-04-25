b34fcd5c57217671d248b5b1c8487af001d423f5
Caio N <caioncaminha@gmail.com>
2025-04-24 22:59:08 -0300

    Python exercises 04-24-2025

--- Python/04-10-2025 exercises.py
+++ Python/04-10-2025 exercises.py
@@ -18,23 +18,97 @@ d. Sexo: 'f' ou 'm';
 e. Estado Civil: 's', 'c', 'v', 'd';
 '''
 
+def cadastro():
+    nome = input("Diga seu nome: ")
+    while len(nome) < 3:
+        nome = input("Diga seu nome: ")
+    
+    while True:
+        idade = input("Diga sua idade: ")
+        if idade.isnumeric():
+            idade = int(idade)
+            if idade > 0 and idade < 150:
+                break
+    
+    salario = input("Diga seu salário: ")
+    while not salario.isnumeric():
+        salario = int(salario)
+        salario = input("Diga seu salário: ")
+    
+    sexo = input("Diga f ou m: ")
+    while sexo != "f" and sexo != "m":
+        sexo = input("Diga f ou m: ")
+    
+    e_c = input("Diga s,c,v ou d: ")
+    while not (e_c == "s" or e_c == "c" or e_c == "v" or e_c == "d"):
+        e_c = input("Diga s,c,v ou d: ")
+    print(f"{nome}\n{idade}\n{salario}\n{sexo}\n{e_c}")
 
-
-'''
-3- Supondo que a população de um país A seja da ordem de 80000 habitantes com
+'''3- Supondo que a população de um país A seja da ordem de 80000 habitantes com
 uma taxa anual de crescimento de 3% e que a população de B seja 200000
 habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule e
 escreva o número de anos necessários para que a população do país A ultrapasse
 ou iguale a população do país B, mantidas as taxas de crescimento.'''
 
+def populacao():
+    pop_a = 80000
+    pop_b = 200000
+    anos = 0
+    while pop_a <= pop_b:
+        pop_a *= 1.03
+        pop_b *= 1.015
+        anos += 1
+    print(f"A população A se tornará igual ou maior que a população B em {anos} anos.")
+
 '''4- Faça um programa que leia 5 números e informe a soma e a média dos números.'''
+
+def num_soma_med():
+    c = 0
+    soma = 0
+    while c < 5:
+        nota = input(f"Diga a {c+1} nota: ")
+        while not nota.isnumeric():
+            nota = input(f"Diga {c+1} nota: ")
+        nota = int(nota)
+        soma += nota
+        c += 1
+    media = soma/c
+    print(media)
 
 '''5- Faça um programa que receba dois números inteiros e gere os números inteiros
 que estão no intervalo compreendido por eles.'''
 
+def num_inteiros():
+    a = input("Diga o primeiro número: ")
+    while not a.isnumeric():
+        a = input("Diga o primeiro número: ")
+    a = int(a)
+    
+    b = input("Diga o segundo número: ")
+    while not b.isnumeric():
+        b = input("Diga o segundo número: ")
+    b = int(b)
+    
+    if b < a:
+        aux = a
+        a = b
+        b = aux
+    
+    while a <= b:
+        print(a, end=' ')
+        a += 1
+
 '''6- Faça um programa que leia um nome de usuário e a sua senha e não aceite a
 senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a
 pedir as informações.'''
+
+def login():
+    nome = input("Diga seu nome de usuário: ")
+    senha = input("Digite sua senha: ")
+    while nome == senha:
+        print("Usuário e senha devem ser diferentes.")
+        nome = input("Diga seu nome de usuário: ")
+        senha = input("Digite sua senha: ")
 
 '''7- Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número
 inteiro entre 1 a 10. O usuário deve informar de qual numero ele deseja ver a
@@ -45,21 +119,74 @@ Tabuada de 5:
 ...
 5 X 10 = 50'''
 
+def tabuada():
+    num = 1
+    while num <= 10:
+        mult = 1
+        while mult <= 10:
+            print(f"{num}*{mult} = {num*mult}")
+            mult += 1
+        num += 1
+        print()
+
 '''8- A série de Fibonacci é formada pela sequência 1,1,2,3,5,8,13,21,34,55,... Faça um
 programa capaz de gerar a série até o n−ésimo termo.'''
+
+def fibonacci():
+    a = 1
+    b = 1
+    i = 2
+    while i < 7:
+        c = a + b
+        print(c)
+        a = b
+        b = c
+        i += 1
 
 '''9- Faça um programa que peça 10 números inteiros, calcule e mostre a quantidade de
 números pares e a quantidade de números ímpares.'''
 
+def num_parimpar():
+    i = 0
+    pares = 0
+    while i < 10:
+        num = input("Digite um número:\n")
+        while not num.isnumeric():
+            num = input("Digite um número:\n")
+        num = int(num)
+        if num%2 == 0:
+            pares += 1
+        i += 1
+    print(f"Você digitou {pares} pares e {i - pares} ímpares.")
+
 '''10 - Faça um programa que calcule o fatorial de um número inteiro fornecido pelo
 usuário. Ex.: 5!=5.4.3.2.1=120'''
+
+def fatorial():
+    i = 1
+    fatorial = 1
+    while i < 10:
+        i += 1
+        fatorial *= i
+    print(fatorial)
 
 '''11 - Faça um programa que peça um número inteiro e determine se ele é ou não um
 número primo. Um número primo é aquele que é divisível somente por ele mesmo e por 1.'''
 
-'''12 - Faça um programa que calcule o mostre a média aritmética de N notas.'''
+def num_primos():
+    i = 2
+    num = 137
+    while True:
+        print(f"{num}%{i} = {num%i}")
+        if num %i == 0:
+            print("Não é primo")
+            break
+        elif i >= num**(1/2):
+            print("É primo")
+            break
+        i += 1
 
-'''13 - Um funcionário de uma empresa recebe aumento salarial anualmente: Sabe-se
+'''12 - Um funcionário de uma empresa recebe aumento salarial anualmente: Sabe-se
 que:
 1. Esse funcionário foi contratado em 1995, com salário inicial de R$ 1.000,00;
 2. Em 1996 recebeu aumento de 1,5% sobre seu salário inicial;
@@ -68,11 +195,49 @@ dobro do percentual do ano anterior. Faça um programa que determine o
 salário atual desse funcionário. Após concluir isto, altere o programa
 permitindo que o usuário digite o salário inicial do funcionário.'''
 
-'''14 - Faça um programa que leia uma quantidade indeterminada de números positivos e
+def salarios():
+    salario = 1000
+    ano = 1995
+    taxa_i = 0.015
+    while ano < 2000:
+        taxa = 1 + taxa_i
+        salario *= taxa
+        ano += 1
+    print(salario)
+
+'''13 - Faça um programa que leia uma quantidade indeterminada de números positivos e
 conte quantos deles estão nos seguintes intervalos: [0-25], [26-50], [51-75] e [76-100].
 A entrada de dados deverá terminar quando for lido um número negativo.'''
 
-'''15 - Em uma eleição presidencial existem quatro candidatos. Os votos são informados
+def num_indet():
+    intervalo_1 = 0
+    intervalo_2 = 0
+    intervalo_3 = 0
+    intervalo_4 = 0
+    while True:
+        num = input("Digite um número:\n")
+        while not num.isnumeric():
+            num = input("Digite um número:\n")
+        num = int(num)
+        if num < 25:
+            intervalo_1 += 1
+        elif num < 50:
+            intervalo_2 += 1
+        elif num < 75:
+            intervalo_3 += 1
+        elif num < 100:
+            intervalo_4 += 1
+        else:
+            print("Digite um número entre 0 e 100")
+        break
+        
+    continuar = input("Você quer continuar? s ou n\n")
+    while not (continuar == "s" or continuar == "n"):
+        continuar = input("Você quer continuar? s ou n\n")
+        if continuar == "n":
+            break
+
+'''14 - Em uma eleição presidencial existem quatro candidatos. Os votos são informados
 por meio de código. Os códigos utilizados são:
 1 , 2, 3, 4 - Votos para os respectivos candidatos
 (você deve montar a tabela ex: 1 - Jose/ 2- João/etc)
