f16432bfd1ca023b5e7ab11949aab00c35ff5f68
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-01 20:52:24 -0300

    .

--- Python/08-28-2025.py
+++ Python/08-28-2025.py
@@ -28,3 +28,25 @@ def matriz_diagonal_prof(tamanho):
     for i in range(tamanho):
         matriz[i][i] = 'x'
     return matriz
+
+def diagonal_inversa_prof(tamanho):
+    matriz = criar_matriz(tamanho,tamanho)
+    for i in range(tamanho):
+        matriz[i][tamanho - i - 1] = 'x'
+    return matriz
+
+def matriz_x(tamanho):
+    mat = criar_matriz(tamanho,tamanho)
+    for i in range(tamanho):
+        mat[i][i] = 'x'
+        mat[i][tamanho - i - 1] = 'x'
+    return mat
+
+def matriz_x(linhas, colunas):
+    mat = criar_matriz(linhas, colunas)
+    for i in range(colunas):
+        mat[0][i] = 'x'
+        mat[linhas - 1][i] = 'x'
+        mat[i][0] = 'x'
+        mat[i][colunas - 1] = 'x'
+    return mat
