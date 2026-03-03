e022b904a3b6a5ab8f285ce940c82019837f5dc3
Caioncaminha <caioncaminha@gmail.com>
2026-03-02 21:46:28 -0300

    Initialize aula2 project structure and add Teste class

---
+++ year-two/poo-java/aula2/src/aula2/Teste.java
@@ -0,0 +1,10 @@
+package aula2;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bem vindo(a) a aula 2, Caio!");
+	}
+
+}
